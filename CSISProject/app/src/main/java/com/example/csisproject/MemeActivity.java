30cf2cff8acade40321110fea30168f57e1d25d7
Manny Kim <namu6688@gmail.com>
2019-11-22 23:02:22 -0800

    MemeActivity Done.

--- CSISProject/app/src/main/java/com/example/csisproject/MemeActivity.java
+++ CSISProject/app/src/main/java/com/example/csisproject/MemeActivity.java
@@ -1,76 +1,118 @@
 package com.example.csisproject;
 
-import android.content.Context;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.os.Environment;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MemeActivity extends AppCompatActivity {
-    String imageUrl;
-    Button saveBtn;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meme);
-        // !!!get selected url from intent!!!
+
+        verifyStoragePermissions(MemeActivity.this); // prompt user write-permission if not yet granted
+
+        // !!!get selected url from intent!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!use putextra from last activity!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         // imageUrl = ...;
 
         // Display Image
-        imageUrl = "https://i.imgur.com/tGbaZCY.jpg"; // [TEST IMAGE]
-        ImageView imgView = findViewById(R.id.imgViewLarge);
+        final String imageUrl = "https://i.imgur.com/tGbaZCY.jpg"; // [TEST IMAGE]
+        final ImageView imgView = findViewById(R.id.imgViewLarge);
         Picasso.with(this).load(imageUrl).into(imgView);
 
         // Save Image
-        saveBtn = findViewById(R.id.btnSaveGallery);
+        Button saveBtn = findViewById(R.id.btnSaveGallery);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    saveImage(imageUrl);
+                    saveImage(imgView, parseUrl(imageUrl));
                 } catch (Exception e) {
-                    Log.e("MANNY:",e.getMessage());
+                    Log.e("MANNY:", e.getMessage());
+                }
+            }
+        });
+
+        // share Image
+        Button shareBtn = findViewById(R.id.btnShareImg);
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("image/jpg");
+                    Log.d("MANNY:", Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/" + parseUrl(imageUrl));
+                    StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                    StrictMode.setVmPolicy(builder.build());
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/" + parseUrl(imageUrl))));
+                    startActivity(Intent.createChooser(shareIntent, "Share this meme with..."));
+                } catch (Exception e) {
+                    Log.e("MANNY:", e.getMessage());
                 }
             }
         });
     }
 
-    private void saveImage(String imageUrl) throws IOException {
-        URL url = new URL (imageUrl);
-        InputStream input = url.openStream();
-        try {
-            //The sdcard directory e.g. '/sdcard' can be used directly, or
-            //more safely abstracted with getExternalStorageDirectory()
-            File storagePath = Environment.getExternalStorageDirectory();
-            OutputStream output = new FileOutputStream (storagePath + "/myImage2.jpg");
-            try {
-                byte[] buffer = new byte[2048];
-                int bytesRead = 0;
-                while ((bytesRead = input.read(buffer, 0, buffer.length)) >= 0) {
-                    output.write(buffer, 0, bytesRead);
-                }
-            } finally {
-                output.close();
-            }
-        } finally {
-            input.close();
+    private void saveImage(View v,String filename){
+        String StoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String savePath = StoragePath + "/Download";
+        File f = new File(savePath);
+        if (!f.isDirectory()) f.mkdirs();
+        v.buildDrawingCache();
+        Bitmap bitmap = v.getDrawingCache();
+        FileOutputStream fos;
+
+        try{
+            fos = new FileOutputStream(savePath+"/"+filename);
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos);
+            Toast.makeText(this, "Image Saved Successfully", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Toast.makeText(this, "Image NOT Saved", Toast.LENGTH_SHORT).show();
+            Log.e("MANNY:", e.getMessage());
         }
+    }
+
+    // get write external storage permission
+    private void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    // get unique id from url
+    private String parseUrl(String url) {
+        String[] tokens = url.split("/");
+        return tokens[3];
     }
 }
